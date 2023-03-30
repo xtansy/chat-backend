2d7067a1bd735e0d2bcd30857b7789df37aae652
xtansy <xtansy@mail.ru>
2023-03-30 17:16:58 +0300

    add netlify toml

--- dist/controllers/socket/socketIdAndUserId.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userIdAndSocketId = void 0;
-exports.userIdAndSocketId = new Map();
--- dist/models/role.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Role = void 0;
-const mongoose_1 = require("mongoose");
-exports.Role = (0, mongoose_1.model)("Role", new mongoose_1.Schema({
-    name: String,
-}, {
-    versionKey: false
-}));
